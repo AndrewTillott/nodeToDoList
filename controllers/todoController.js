0746a198d13bda0443e1a7bbf72b98ece9d32363
AndrewTillott <andytillott@hotmail.co.uk>
2020-09-03 17:29:38 +0100

    add dummy data to a database with mongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,6 +2,24 @@
 //e.g. manipulate data, handle routes etc
 
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to the database 
+mongoose.connect('mongodb://test:test@ds017195.mlab.com:17195/todo')
+
+//Create a schema - like a blueprint 
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+//the 'Todo' in the parentheses is the model name that will be stored in a collection in mongoDB
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({
+    item: 'buy flowers'
+}).save(function(err){
+    if(err) throw err;
+    console.log('item saved');
+});
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'code'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -18,7 +36,10 @@ module.exports = function(app){
         res.json(data);
     });
 
-    app.delete('/todo', function(req, res){
-
+    app.delete('/todo/:item', function(req, res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json('data');
     });
 };
