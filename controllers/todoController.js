595ac9739c91fd07710959b04fdb015cd60a10a2
AndrewTillott <andytillott@hotmail.co.uk>
2020-09-03 17:40:51 +0100

    push and delete requests now interact with the database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -14,32 +14,49 @@ var todoSchema = new mongoose.Schema({
 
 //the 'Todo' in the parentheses is the model name that will be stored in a collection in mongoDB
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({
+/*var itemOne = Todo({
     item: 'buy flowers'
 }).save(function(err){
     if(err) throw err;
     console.log('item saved');
-});
+});*/
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'code'}];
+//var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'code'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 //we are passing in the app from app.js
 module.exports = function(app){
     app.get('/todo', function(req, res){
-        res.render('todo', {todos: data});
+        //get data from mongodb and pass it to the view 
+        //find can find one object or all of them (empty object is to get all in the todo collection)
+        Todo.find({}, function(err, data){
+            if(err) throw err;
+            res.render('todo', {todos: data});
+        })
+       //alternative: Todo.find({item: 'buy  flowers'}); 
+       // res.render('todo', {todos: data});
     });
 
     app.post('/todo', urlencodedParser,  function(req, res){
-        data.push(req.body);
-        //send the data back as JSON
+        //get data from the view and add it to mongodb
+        var newTodo = Todo(req.body).save(function(err,data){
+            if(err) throw err;
+            //send the data back as JSON
         res.json(data);
+        })
+        //data.push(req.body);
+       
     });
 
     app.delete('/todo/:item', function(req, res){
-        data = data.filter(function(todo){
+        //delete the requested item from mongodb
+        Todo.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err, data){
+            if(err) throw err;
+            res.json(data);
+        })
+        /*data = data.filter(function(todo){
             return todo.item.replace(/ /g, '-') !== req.params.item;
         });
-        res.json('data');
+        res.json('data');*/
     });
 };
